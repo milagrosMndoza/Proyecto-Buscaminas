7facd20680a7ccaeb390327998cb9b1a01b5f1c3
milagrosMndoza <milagrosmndoza@gmail.com>
2017-09-20 09:52:58 -0500

    modificando mi archivo js poo clases

--- assets/js/solucion_poo_clases.js
+++ assets/js/solucion_poo_clases.js
@@ -1,18 +1,12 @@
 //solucion_poo_clases.js
 'use strict';
-const app = {
-    item: {
+class player {
+    constructor(minas) {
 
         minas: undefined,
 
-    },
-    init: function(){
-         app.item.minas= app.inicializaMatriz();
-         app.crearTablero();
-         app.generarBombas(app.item.minas);
-         app.bombasAlrededor(app.item.minas);
-    },   
-    inicializaMatriz: function () {
+
+    inicializaMatriz () {
             let tabla = [];
             for (let i = 0; i < 8; i++) {
                 tabla[i] = [0, 0, 0, 0, 0, 0, 0, 0];
@@ -21,33 +15,33 @@ const app = {
         },      
         
         
-         crearTablero: function() {
+         crearTablero() {
             for (let i = 0; i < 8; i++) {
                 for (let j = 0; j < 8; j++) {
                     let div = document.createElement("div");
                     div.id = i + "" + j;
-                    div.addEventListener("click", app.mostrarNumero, true);
+                    div.addEventListener("click",  mostrarNumero, true);
                     tablerominas.appendChild(div);
                 }
             }
         
         },
         
-         mostrarNumero:function(e) {
+         mostrarNumero(e) {
             let auxstr = this.id.split("");
             let myid = auxstr[0] + auxstr[1];
             let divObj = document.getElementById(myid);
         
-            if (app.item.minas[parseInt(auxstr[0], 10)][parseInt(auxstr[1], 10)] == 0) {
+            if ( item.minas[parseInt(auxstr[0], 10)][parseInt(auxstr[1], 10)] == 0) {
                 divObj.style.backgroundColor = "white";
-                app.abrirAlrededor(parseInt(auxstr[0], 10), parseInt(auxstr[1], 10), app.item.minas);
+                 abrirAlrededor(parseInt(auxstr[0], 10), parseInt(auxstr[1], 10),  item.minas);
             } else {
-                if (app.item.minas[parseInt(auxstr[0], 10)][parseInt(auxstr[1], 10)] != "*") {
-                    document.getElementById(myid).innerHTML = "<p style='margin-top:15px;'>" + app.item.minas[parseInt(auxstr[0], 10)][parseInt(auxstr[1], 10)] + "</p>";
+                if ( item.minas[parseInt(auxstr[0], 10)][parseInt(auxstr[1], 10)] != "*") {
+                    document.getElementById(myid).innerHTML = "<p style='margin-top:15px;'>" +  item.minas[parseInt(auxstr[0], 10)][parseInt(auxstr[1], 10)] + "</p>";
                     divObj.style.backgroundColor = "white";
                 } else {
                     divObj.style.backgroundImage = "url(assets/img/bomba.gif)";
-                    app.abrirTablero(app.item.minas);
+                     abrirTablero( item.minas);
                     swal(
                         'Oops...',
                         'Perdiste! Esto ha explotado!',
@@ -57,35 +51,35 @@ const app = {
             }
         },
         
-         bombasAlrededor: function(tablero) {
+         bombasAlrededor(tablero) {
             for (let i = 0; i < 8; i++) {
                 for (let j = 0; j < 8; j++) {
                     if (tablero[i][j] == "*") {
                         if (i == 0 && j == 0) {
                             colocaNumeroBombas(i, j, i + 1, j + 1, tablero);
                         } else if (i == 0 && (j > 0 && j < 7)) {
-                            app.colocaNumeroBombas(i, j - 1, i + 1, j + 1, tablero);
+                             colocaNumeroBombas(i, j - 1, i + 1, j + 1, tablero);
                         } else if (i == 0 && j == 7) {
-                            app.colocaNumeroBombas(i, j - 1, i + 1, j, tablero);
+                             colocaNumeroBombas(i, j - 1, i + 1, j, tablero);
                         } else if (j == 7 && (i > 0 && i < 7)) {
-                            app.colocaNumeroBombas(i - 1, j - 1, i + 1, j, tablero);
+                             colocaNumeroBombas(i - 1, j - 1, i + 1, j, tablero);
                         } else if (i == 7 && j == 7) {
-                            app.colocaNumeroBombas(i - 1, j - 1, i, j, tablero);
+                             colocaNumeroBombas(i - 1, j - 1, i, j, tablero);
                         } else if (i == 7 && (j > 0 && j < 7)) {
-                            app.colocaNumeroBombas(i - 1, j - 1, i, j + 1, tablero);
+                             colocaNumeroBombas(i - 1, j - 1, i, j + 1, tablero);
                         } else if (i == 7 && j == 0) {
-                            app.colocaNumeroBombas(i - 1, j, i, j + 1, tablero);
+                             colocaNumeroBombas(i - 1, j, i, j + 1, tablero);
                         } else if (j == 0 && (i > 0 && i < 7)) {
-                            app.colocaNumeroBombas(i - 1, j, i + 1, j + 1, tablero);
+                             colocaNumeroBombas(i - 1, j, i + 1, j + 1, tablero);
                         } else {
-                            app.colocaNumeroBombas(i - 1, j - 1, i + 1, j + 1, tablero);
+                             colocaNumeroBombas(i - 1, j - 1, i + 1, j + 1, tablero);
                         }
                     }
                 }
             }
         },
         
-         colocaNumeroBombas: function(leti, letj, fini, finj, tablero) {
+         colocaNumeroBombas(leti, letj, fini, finj, tablero) {
             for (let i = leti; i <= fini; i++) {
                 for (let j = letj; j <= finj; j++) {
                     if (tablero[i][j] != "*") {
@@ -95,7 +89,7 @@ const app = {
             }
         },
         
-         generarBombas: function(tablero) {
+         generarBombas(tablero) {
             let fil = 0;
             let col = 0;
         
@@ -111,7 +105,7 @@ const app = {
             }
         },
         
-         abrirCeros: function(leti, letj, fini, finj, cori, corj, tablero) {
+         abrirCeros(leti, letj, fini, finj, cori, corj, tablero) {
             for (let i = leti; i <= fini; i++) {
                 for (let j = letj; j <= finj; j++) {
                     let myid = i + "" + j;
@@ -123,7 +117,7 @@ const app = {
                                 objDiv.style.backgroundColor = "white";
                             } else {
                                 if (objDiv.style.backgroundColor != "white") {
-                                    app.abrirAlrededor(i, j, tablero);
+                                     abrirAlrededor(i, j, tablero);
                                 }
                             }
         
@@ -138,29 +132,29 @@ const app = {
             }
         },
         
-         abrirAlrededor: function(xi, xj, tablero) {
+         abrirAlrededor(xi, xj, tablero) {
             if (xi == 0 && xj == 0) {
-                app.abrirCeros(xi, xj, xi + 1, xj + 1, xi, xj, tablero);
+                 abrirCeros(xi, xj, xi + 1, xj + 1, xi, xj, tablero);
             } else if (xi == 0 && (xj > 0 && xj < 7)) {
-                app.abrirCeros(xi, xj - 1, xi + 1, xj + 1, xi, xj, tablero);
+                 abrirCeros(xi, xj - 1, xi + 1, xj + 1, xi, xj, tablero);
             } else if (xi == 0 && xj == 7) {
-                app.abrirCeros(xi, xj - 1, xi + 1, xj, xi, xj, tablero);
+                 abrirCeros(xi, xj - 1, xi + 1, xj, xi, xj, tablero);
             } else if (xj == 7 && (xi > 0 && xi < 7)) {
-                app.abrirCeros(xi - 1, xj - 1, xi + 1, xj, xi, xj, tablero);
+                 abrirCeros(xi - 1, xj - 1, xi + 1, xj, xi, xj, tablero);
             } else if (xi == 7 && xj == 7) {
-                app.abrirCeros(xi - 1, xj - 1, xi, xj, xi, xj, tablero);
+                 abrirCeros(xi - 1, xj - 1, xi, xj, xi, xj, tablero);
             } else if (xi == 7 && (xj > 0 && xj < 7)) {
-                app.abrirCeros(xi - 1, xj - 1, xi, xj + 1, xi, xj, tablero);
+                 abrirCeros(xi - 1, xj - 1, xi, xj + 1, xi, xj, tablero);
             } else if (xi == 7 && xj == 0) {
-                app.abrirCeros(xi - 1, xj, xi, xj + 1, xi, xj, tablero);
+                 abrirCeros(xi - 1, xj, xi, xj + 1, xi, xj, tablero);
             } else if (xj == 0 && (xi > 0 && xi < 7)) {
-                app.abrirCeros(xi - 1, xj, xi + 1, xj + 1, xi, xj, tablero);
+                 abrirCeros(xi - 1, xj, xi + 1, xj + 1, xi, xj, tablero);
             } else {
-                app.abrirCeros(xi - 1, xj - 1, xi + 1, xj + 1, xi, xj, tablero);
+                 abrirCeros(xi - 1, xj - 1, xi + 1, xj + 1, xi, xj, tablero);
             }
         },
         
-         abrirTablero: function(tablero) {
+         abrirTablero(tablero) {
             for (let i = 0; i < 8; i++) {
                 for (let j = 0; j < 8; j++) {
                     let myid = i + "" + j;
@@ -173,5 +167,10 @@ const app = {
         }
         
 }
-
-app.init();
+    
+init(){
+    item.minas=  inicializaMatriz();
+    crearTablero();
+    generarBombas( minas);
+    bombasAlrededor( minas);
+}
